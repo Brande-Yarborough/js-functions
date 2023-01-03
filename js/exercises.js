56b5bcd831620249146cbd8b7b8adfbf194925f9
BrandeYarborough <brandeyarborough@gmail.com>
2023-01-03 18:23:08 -0500

    complete is vowel

--- js/exercises.js
+++ js/exercises.js
@@ -1,60 +1,45 @@
-(function() {
-  'use strict';
+(function () {
+  "use strict";
 
   // ---------------------
   // Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in Javascript.
   // ---------------------
 
-function max(num1, num2) {
-  //calculates largest of two numbers//
-  if(num1 > num2) {
-    return num1;
-  } else {
-    return num2;
-    //returns largest number if true, returns smallest number if false//
+  function max(num1, num2) {
+    //calculates largest of two numbers//
+    if (num1 > num2) {
+      return num1;
+    } else {
+      return num2;
+      //returns largest number if true, returns smallest number if false//
+    }
   }
-}
 
   // ---------------------
   // Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
   // ---------------------
-// 
+  //
 
   function maxOfThree(num1, num2, num3) {
     //calculates the largest of three numbers//
     return Math.max(num1, num2, num3);
     //Math.max will return the largest of three numbers//
-
   }
-
 
   // ---------------------
   // Define a function isVowel() that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
   // ---------------------
-  function isVowel(argument){
+  function isVowel(letter) {
+    let result; //let is initializing variable//
+    if (letter === "a" || letter === "e" || letter === "i" || letter === "o" || letter === "u") {
+      //letter is what is getting passed into function. comparing that to each vowel//
+      result = true;
+    } else {
+      result = false;
+    }
+    return result; //result of if else statement//
+  }
 
-    var text;
-    var argument = argument.toLowerCase();
-  
-      var vowels = (['a', 'e', 'i', 'o', 'u']);
-  
-      for (var i = 0; i <= vowels.length; i++){
-          if (argument != vowels[i]) {
-              continue;
-          }
-          return true;
-      }
-      return false;
-  }
-  
-  // See if "A" is a vowel
-  var char = "A";
-  
-  if (isVowel(char)) {
-    console.log(char + " is a vowel");
-  } else {
-    console.log(char + " is not a vowel");
-  }
   /*function isVowel(x) {
     if(x=a) {
       return true;
@@ -67,74 +52,100 @@ function max(num1, num2) {
 
   }*/
 
-
   // ---------------------
   // Write a function rovarspraket() that will translate a text into "rövarspråket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
   // ---------------------
-
 
   // ---------------------
   // Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sum([1,2,3,4]) should return 10, and multiply([1,2,3,4]) should return 24.
   // ---------------------
 
-
-
   // ---------------------
   // Define a function reverse() that computes the reversal of a string. For example, reverse("jag testar") should return the string "ratset gaj".
   // ---------------------
-
-
 
   // ---------------------
   // Define a function findLongestWord() that takes an array of words and returns the length of the longest one.
   // ---------------------
 
-
-
   // ---------------------
   // Define a function filterLongWords() that takes an array of words and an integer i and returns the array of words that are longer than i.
   // ---------------------
-
-
 
   // ---------------------
   // Define a function charFreq() that takes a string and builds a frequency listing of the characters contained in it. Represent the frequency listing as a Javascript object. Try it with something like charFreq("abbabcbdbabdbdbabababcbcbab").
   // ---------------------
 
-
   ////////////////////////////////////////////////////////////////////////
   /////////////////////////DO NOT CHANGE CODE BELOW///////////////////////
   ////////////////////////////////////////////////////////////////////////
 
-  console.assert(max(2, 4) === 4, 'function max error');
+  console.assert(max(2, 4) === 4, "function max error");
 
-  console.assert(maxOfThree(1, 2, 3) === 3, 'ERROR funtion maxOfThree');
+  console.assert(maxOfThree(1, 2, 3) === 3, "ERROR funtion maxOfThree");
 
-  console.assert(maxOfThree(2, 3, 1) === 3, 'ERROR funtion maxOfThree');
+  console.assert(maxOfThree(2, 3, 1) === 3, "ERROR funtion maxOfThree");
 
-  console.assert(maxOfThree(3, 2, 1) === 3, 'ERROR maxOfThree');
+  console.assert(maxOfThree(3, 2, 1) === 3, "ERROR maxOfThree");
 
-  console.assert(isVowel('a') === true, 'ERROR function isVowel');
+  console.assert(isVowel("a") === true, "ERROR function isVowel");
 
-  console.assert(isVowel('b') == false, 'ERROR function isVowel');
+  console.assert(isVowel("b") == false, "ERROR function isVowel");
 
-  console.assert(rovarspraket("this is fun") === "tothohisos isos fofunon", 'ERROR function rovarspraket');
+  console.assert(
+    rovarspraket("this is fun") === "tothohisos isos fofunon",
+    "ERROR function rovarspraket"
+  );
 
-  console.assert(sum([1, 2, 3, 4]) === 10, 'ERROR function sum');
+  console.assert(sum([1, 2, 3, 4]) === 10, "ERROR function sum");
 
-  console.assert(multiply([1, 2, 3, 4]) === 24, 'ERROR function multiply');
+  console.assert(multiply([1, 2, 3, 4]) === 24, "ERROR function multiply");
 
-  console.assert(reverse('jag testar') === 'ratset gaj', 'ERROR function reverse');
+  console.assert(
+    reverse("jag testar") === "ratset gaj",
+    "ERROR function reverse"
+  );
 
-  console.assert(findLongestWord(['hello', 'world', 'does','anyone', 'really', 'know', 'what', 'time', 'it', 'is']) === 6, 'ERROR function findLongestWord');
+  console.assert(
+    findLongestWord([
+      "hello",
+      "world",
+      "does",
+      "anyone",
+      "really",
+      "know",
+      "what",
+      "time",
+      "it",
+      "is",
+    ]) === 6,
+    "ERROR function findLongestWord"
+  );
 
-  console.assert(filterLongWords(['hello', 'world', 'does','anyone', 'really', 'know', 'what', 'time', 'it', 'is'], 4).length === 4, 'ERROR function filterLongWords');
+  console.assert(
+    filterLongWords(
+      [
+        "hello",
+        "world",
+        "does",
+        "anyone",
+        "really",
+        "know",
+        "what",
+        "time",
+        "it",
+        "is",
+      ],
+      4
+    ).length === 4,
+    "ERROR function filterLongWords"
+  );
 
-  console.assert(charFreq('abbabcbdbabdbdbabababcbcbab').a === 7);
+  console.assert(charFreq("abbabcbdbabdbdbabababcbcbab").a === 7);
 
-  console.assert(charFreq('abbabcbdbabdbdbabababcbcbab').b === 14);
+  console.assert(charFreq("abbabcbdbabdbdbabababcbcbab").b === 14);
 
-  console.assert(charFreq('abbabcbdbabdbdbabababcbcbab').c === 3);
+  console.assert(charFreq("abbabcbdbabdbdbabababcbcbab").c === 3);
 
-  console.assert(charFreq('abbabcbdbabdbdbabababcbcbab').d === 3);
+  console.assert(charFreq("abbabcbdbabdbdbabababcbcbab").d === 3);
 })();
